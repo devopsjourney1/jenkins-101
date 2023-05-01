ff36efcc3c6d2fb6c9709fd4cf9e7f1fab025a53
Alfredo Martinez <alfredo_martinez_1@yahoo.com>
2023-04-30 21:07:25 -0700

    Fixed typo

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,7 @@
 import fire
 
 def hello(name="World"):
-  return "Hello %s! Welcome to my worlkd" % name
+  return "Hello %s! Welcome to my world" % name
 
 if __name__ == '__main__':
   fire.Fire(hello)
