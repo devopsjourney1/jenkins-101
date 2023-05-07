32b1a7edf0cfd187e990b726839410974d0f9c97
Alfredo Martinez <alfredo_martinez_1@yahoo.com>
2023-05-07 14:30:54 -0700

    Added sonarqube and gitignore

--- myapp/hello.py
+++ myapp/hello.py
@@ -2,6 +2,11 @@ import fire
 
 def hello(name="World"):
   return "Hello %s! Welcome to my world! " % name
-
+def test(name):
+  if name:
+    return True
+  else:
+    return False
+  retrun "None"
 if __name__ == '__main__':
   fire.Fire(hello)
