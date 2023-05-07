2b638b49f7f49e4af3227b3fafe0552e0eac9104
Alfredo Martinez <alfredo_martinez_1@yahoo.com>
2023-05-07 15:42:40 -0700

    updated

--- myapp/hello.py
+++ myapp/hello.py
@@ -2,11 +2,14 @@ import fire
 
 def hello(name="World"):
   return "Hello %s! Welcome to my world! " % name
+
+
 def test(name):
   if name:
     return True
   else:
     return False
-  retrun "None"
+  retrun True
+  
 if __name__ == '__main__':
   fire.Fire(hello)
