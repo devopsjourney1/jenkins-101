b941dac16eaa0a13e44b69839b73a9cba9b05e4f
Alfredo Martinez <alfredo_martinez_1@yahoo.com>
2023-05-09 21:05:23 -0700

    added sonar in Jenkins file

--- myapp/hello.py
+++ myapp/hello.py
@@ -9,7 +9,14 @@ def test(name):
     return True
   else:
     return False
+  
   retrun True
   
+def test2(name):
+  print(f'hello {name}')
+
+def test3(name):
+  print(f'hello {name}')
+
 if __name__ == '__main__':
   fire.Fire(hello)
